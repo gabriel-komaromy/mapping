6a168456d1abcfb511ad8065e6cc61c01a10fe24
komaromy <gre5@case.edu>
2016-04-02 21:11:05 -0400

    began agent
    Signed-off-by: komaromy <gre5@case.edu>

---
+++ mapping_agent.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+from q_learning_agent import Agent
+from agents import SingleAgentID
+from actions import ActionMap
+from actions import Action
+from actions import NumericActionComponent
+from actions import ComponentName
+from observations import FeatureName
+
+
+class MappingAgent(Agent):
+    feature_names = {
+        'x': FeatureName('x'),
+        'y': FeatureName('y'),
+        'north': FeatureName('north'),
+        'east': FeatureName('east'),
+        'south': FeatureName('south'),
+        'west': FeatureName('west'),
+        }
+
+    component_names = {
+        'x': ComponentName('x'),
+        'y': ComponentName('y'),
+        }
+
+    BINS_PER_DIMENSION = 30
+    def __init__(self, environment_spec):
+        print 'initializing agent'
+        action_descriptors = environment_spec.action_descriptors
+        self.x_component_descriptor = action_descriptors.descriptors[self.component_names['x']]
+        self.y_component_descriptor = action_descriptors.descriptors[self.component_names['y']]
+
+        self.dimensions = (self.x_component_descriptor.boundaries()[1], self.y_component_descriptor.boundaries()[1])
+
+        self.agent_id = SingleAgentID()
+        self.proba_map = np.zeros((BINS_PER_DIMENSION, BINS_PER_DIMENSION))
+        self.observed__map = np.zeros((BINS_PER_DIMENSION, BINS_PER_DIMENSION))
+
+    def update(self, agent_update):
+        observation_map = agent_update.observation_map
+        observation = observation_map[self.agent_id]
+        self.position = (observation.get_value(self.component_names['x']), observation.get_value(self.component_names['y']))
+        action_map = ActionMap()
+        action = Action()
+        next_x, next_y = self.next_movement()
+        x_component = NumericActionComponent(next_x)
+        action.add_component(self.component_names['x'], x_component)
+        y_component = NumericActionComponent(next_y)
+        action.add_component(self.component_names['y'], y_component)
+        action_map.add_action(self.agent_id, action)
+        return action_map
